f0007e31d0fd3af4c8444260ebdc64ea7ac0d9b5
Nephat Mutugi <muchirinephat5@gmail.com>
2020-04-20 23:16:12 +0300

    Activities layout files

--- app/src/main/java/com/nephat/watotowetu/authentication/LoginActivity.java
+++ app/src/main/java/com/nephat/watotowetu/authentication/LoginActivity.java
@@ -3,27 +3,62 @@ package com.nephat.watotowetu.authentication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.nephat.watotowetu.MainActivity;
 import com.nephat.watotowetu.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
 
+    //Widgets
+    private EditText mPhoneNumber;
+    private EditText mPassword;
+    private Button mLogin;
+    private TextView mLinkForgotPassword;
+    private TextView mLinkRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mPhoneNumber = findViewById(R.id.phoneNumber);
+        mPassword = findViewById(R.id.inputPass);
+        mLogin = findViewById(R.id.buttonLogin);
+        mLinkForgotPassword = findViewById(R.id.forgotPassText);
+        mLinkRegister = findViewById(R.id.linkRegister);
 
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mLinkForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mLinkRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
 }
--- app/src/main/java/com/nephat/watotowetu/authentication/RegisterActivity.java
+++ app/src/main/java/com/nephat/watotowetu/authentication/RegisterActivity.java
@@ -2,15 +2,88 @@ package com.nephat.watotowetu.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.nephat.watotowetu.MainActivity;
 import com.nephat.watotowetu.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //Widgets
+    private EditText fName;
+    private EditText lName;
+    private EditText mPhone;
+    private EditText mPassword;
+    private EditText mConfirmPass;
+    private Button mRegBtn;
+    private TextView mLinkLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fName = findViewById(R.id.firstName);
+        lName = findViewById(R.id.lastName);
+        mPhone = findViewById(R.id.enterPhone);
+        mPassword = findViewById(R.id.enterPass);
+        mConfirmPass = findViewById(R.id.confirmPass);
+        mRegBtn = findViewById(R.id.regBtn);
+        mLinkLogin = findViewById(R.id.linkLogin);
+
+        mLinkLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String first_name = fName.getText().toString().trim();
+                String last_name = lName.getText().toString().trim();
+                String phone = mPhone.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                String confirm_pass = mConfirmPass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(first_name)){
+                    fName.setError("First name is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(last_name)){
+                    lName.setError("Last name is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(phone)){
+                    mPhone.setError("Phone number is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required");
+                    return;
+                }
+                if (password.length()<6){
+                    mPassword.setError("Password should be 6 or more characters");
+                    return;
+                }
+                if (!TextUtils.equals(password,confirm_pass)){
+                    mConfirmPass.setError("Passwords do not match");
+                }else{
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
     }
 }
